8c84c4838044e893253466a210ae49be79a547a9
hont127@163.com <hont127@163.com>
2019-01-26 13:09:11 +0800

    Signed-off-by: hont127@163.com <hont127@163.com>

---
+++ GGJ2019/Assets/Scripts/Hont/CharacterItemSO.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "ScriptableObject/CharacterItemSO")]
+public class CharacterItemSO : ScriptableObject
+{
+    [Serializable]
+    public class ItemInfo
+    {
+        public UnitSO unit;
+        public GameObject targetItem;
+        public Vector3 position;
+    }
+
+    public List<ItemInfo> items = new List<ItemInfo>();
+}
