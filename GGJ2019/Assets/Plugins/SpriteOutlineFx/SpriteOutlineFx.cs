b2e92b59c7d50f0751d05a40224af883965db29e
hont127@163.com <hont127@163.com>
2019-01-27 09:44:52 +0800

    Signed-off-by: hont127@163.com <hont127@163.com>

---
+++ GGJ2019/Assets/Plugins/SpriteOutlineFx/SpriteOutlineFx.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine.Rendering;
+
+[ExecuteInEditMode]
+[RequireComponent(typeof(Camera))]
+[AddComponentMenu("Image Effects/2D Sprite/Outline")]
+public class SpriteOutlineFx : MonoBehaviour
+{
+    [SerializeField] Color _outlineColor = new Color(128f / 255f, 0f, 0f, 1f);
+
+    public Color outlineColor
+    {
+        get { return _outlineColor; }
+        set { _outlineColor = value; }
+    }
+
+    [SerializeField] float _sampleDistance = 2;
+
+    public float sampleDistance
+    {
+        get { return _sampleDistance; }
+        set { _sampleDistance = value; }
+    }
+
+    [SerializeField] Shader shader;
+
+    Material _material;
+    CommandBuffer mCB;
+
+    public static List<SpriteOutlineItem> outlineItemList = new List<SpriteOutlineItem>();
+
+
+    void Update()
+    {
+        var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        var hit = default(RaycastHit);
+        if (Physics.Raycast(ray, out hit, 1 << 8))
+        {
+            hit.transform.GetComponent<SpriteOutlineItem>().enabled = true;
+        }
+        else
+        {
+            for (int i = outlineItemList.Count - 1; i >= 0; i--)
+            {
+                outlineItemList[i].enabled = false;
+            }
+            outlineItemList.Clear();
+        }
+    }
+
+    void Setup()
+    {
+        if (_material == null)
+        {
+            _material = new Material(shader);
+            _material.hideFlags = HideFlags.DontSave;
+        }
+    }
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Setup();
+
+        _material.SetFloat("_Distance", _sampleDistance);
+        _material.SetColor("_Color", _outlineColor);
+
+        if (mCB == null)
+            mCB = new CommandBuffer();
+
+        mCB.Clear();
+        foreach (var item in outlineItemList)
+        {
+            var renderer = item.GetComponentInParent<Renderer>();
+            if (renderer)
+                mCB.DrawRenderer(renderer, _material);
+        }
+
+        Graphics.Blit(source, destination);
+        Graphics.ExecuteCommandBuffer(mCB);
+    }
+}
---
+++ GGJ2019/Assets/Plugins/SpriteOutlineFx/SpriteOutlineItem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteOutlineItem : MonoBehaviour
+{
+    void Awake()
+    {
+        if (GameObject.FindObjectOfType<SpriteOutlineFx>() == null)
+            Camera.main.gameObject.AddComponent<SpriteOutlineFx>();
+
+        enabled = false;
+    }
+
+    void OnEnable()
+    {
+        SpriteOutlineFx.outlineItemList.Add(this);
+    }
+
+    void OnDisable()
+    {
+        SpriteOutlineFx.outlineItemList.Remove(this);
+    }
+}
